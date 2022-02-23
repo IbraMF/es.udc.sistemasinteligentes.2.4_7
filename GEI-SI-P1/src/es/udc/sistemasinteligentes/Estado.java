3ceadbd9866ec5fafbb2ffe0ebe80b4e13de49df
Ibrahim Mendez Fernandez <i.mendezf@udc.es>
2022-02-23 08:29:59 +0100

    comentarios

--- GEI-SI-P1/src/es/udc/sistemasinteligentes/Estado.java
+++ GEI-SI-P1/src/es/udc/sistemasinteligentes/Estado.java
@@ -5,11 +5,27 @@ public abstract class Estado {
 
     @Override
     public abstract java.lang.String toString();
+    /* Ejemplo:
+        return "(" + posicionRobot + "," + posicionBasura + ')'
+    */
 
     @Override
     public abstract boolean equals(Object obj);
+    /* Ejemplo:
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EstadoAspiradora that = (EstadoAspiradora) o;
+
+        if (posicionRobot != that.posicionRobot) return false;
+        return posicionBasura == that.posicionBasura;
+    */
 
     @Override
     public abstract int hashCode();
-
+    /* Ejemplo:
+        int result = posicionRobot.hashCode();
+        result = 31 * result + posicionBasura.hashCode();
+        return result;
+    */
 }
