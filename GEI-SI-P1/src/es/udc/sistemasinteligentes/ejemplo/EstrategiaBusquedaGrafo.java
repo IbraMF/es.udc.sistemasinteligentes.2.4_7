d61499fd74aad96e471891f938668d69b5a23151
NicolasrRodriguez <nicolas.rodriguez2@udc.es>
2022-03-02 10:24:44 +0100

    Bsuqueda en grafo comentada

--- GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/EstrategiaBusquedaGrafo.java
+++ GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/EstrategiaBusquedaGrafo.java
@@ -1,29 +1,78 @@
 package es.udc.sistemasinteligentes.ejemplo;
 
-import es.udc.sistemasinteligentes.Accion;
-import es.udc.sistemasinteligentes.Estado;
-import es.udc.sistemasinteligentes.EstrategiaBusqueda;
-import es.udc.sistemasinteligentes.ProblemaBusqueda;
+import es.udc.sistemasinteligentes.*;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class EstrategiaBusquedaGrafo implements EstrategiaBusqueda {
+
+    /*public Queue<Estado> sucesores(ProblemaBusqueda p, Queue<Estado> frontera, Estado estadoactual){
+        Accion[] accionesDisponibles = p.acciones(p.getEstadoInicial());
+        for(Accion accion : accionesDisponibles){
+            if(accion.esAplicable(estadoactual)) //comprobar si esta explorado ??
+                frontera.add(p.result(estadoactual, accion));
+        }
+        return frontera;
+    }*/
 
     /** Cambiar(copiado de estrategia4), aparece en pag56 teoria */
     @Override
     public Nodo[] soluciona(ProblemaBusqueda p) throws Exception {
 
-        ArrayList<Nodo> n = new ArrayList<>();
+        Queue<Estado> frontera = new LinkedList<Estado>();
+        ArrayList<Nodo> listaNodo = new ArrayList<>();
         ArrayList<Estado> explorados = new ArrayList<>();
-        Nodo padre=null;
-        Accion ac;
+        Nodo padre = null;
+        Accion accion;
         Estado estadoActual = p.getEstadoInicial();
-        //explorados.add(estadoActual);
-        ArrayList<Nodo> frontera = new ArrayList<>();
-        frontera.add(new Nodo(estadoActual,null,null));
+
+        explorados.add(estadoActual);
+        //frontera = sucesores(p, frontera, estadoActual);
+
         int i = 1;
 
         System.out.println((i++) + " - Empezando búsqueda en " + estadoActual);
+
+        while (!p.esMeta(estadoActual)) {
+            System.out.println((i++) + " - " + estadoActual + " no es meta");
+            Accion[] accionesDisponibles = p.acciones(estadoActual);
+            boolean modificado = false;
+            for (Accion acc : accionesDisponibles) {
+                Estado sc = p.result(estadoActual, acc);
+                System.out.println((i++) + " - RESULT(" + estadoActual + ","+ acc + ")=" + sc);
+                if (!explorados.contains(sc)) {
+                    estadoActual = sc;
+                    System.out.println((i++) + " - " + sc + " NO explorado");
+                    explorados.add(estadoActual);
+                    //frontera = sucesores(p, frontera, estadoActual);
+                    modificado = true;
+                    System.out.println((i++) + " - Estado actual cambiado a " + estadoActual);
+                    accion = acc;
+                    Nodo nodo = new Nodo(estadoActual,padre,accion);
+                    listaNodo.add(nodo);
+                    padre = nodo;
+                    break;
+                }
+                else
+                    System.out.println((i++) + " - " + sc + " ya explorado");
+            }
+            if(!modificado) throw new Exception("No se ha podido encontrar una solución");
+
+        }
+        System.out.println((i++) + " - FIN - " + estadoActual);
+        Nodo[] arrayNodo = new Nodo[listaNodo.size()]; int j=0;
+        for (Nodo nodo:listaNodo) {
+            arrayNodo[j]= nodo;
+            j++;
+        }
+        return arrayNodo;
+    }
+
+}
+/*System.out.println((i++) + " - Empezando búsqueda en " + estadoActual);
         while (!p.esMeta(estadoActual)){
             if(frontera.isEmpty()){
                 throw new Exception("No se ha podido encontrar una solución");
@@ -58,18 +107,4 @@ public class EstrategiaBusquedaGrafo implements EstrategiaBusqueda {
             if(frontera.size()>0){
                 estadoActual=frontera.get(0).estado;
                 System.out.println((i++) + " - Estado actual cambiado a " + estadoActual);
-            }
-            else{
-                break;
-            }
-
-        }
-        Nodo[] nodos = new Nodo[n.size()]; int j=0;
-        for (Nodo nodo:n) {
-            nodos[j]= nodo;
-            j++;
-        }
-        return nodos;
-    }
-
-}
+            }*/
