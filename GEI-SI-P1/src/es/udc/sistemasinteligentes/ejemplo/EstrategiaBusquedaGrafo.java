d4aaf97bf14a8b642b9ae46ec5b0b50474220696
NicolasrRodriguez <nicolas.rodriguez2@udc.es>
2022-03-12 14:37:42 +0100

    Bsuqueda en grafo hecha(falta comprobar si la lsita de nodos esta bien)

--- GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/EstrategiaBusquedaGrafo.java
+++ GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/EstrategiaBusquedaGrafo.java
@@ -8,60 +8,81 @@ import java.util.List;
 import java.util.Queue;
 
 public class EstrategiaBusquedaGrafo implements EstrategiaBusqueda {
-
-    /*public Queue<Estado> sucesores(ProblemaBusqueda p, Queue<Estado> frontera, Estado estadoactual){
+    public Queue<Nodo> sucesores(ProblemaBusqueda p, Queue<Nodo> frontera, Estado estadoActual,Nodo padre,ArrayList<Estado> Explorados){
+        System.out.println("Expandiendo frontera{");
         Accion[] accionesDisponibles = p.acciones(p.getEstadoInicial());
+        int i=0;
+        Estado sc;
         for(Accion accion : accionesDisponibles){
-            if(accion.esAplicable(estadoactual)) //comprobar si esta explorado ??
-                frontera.add(p.result(estadoactual, accion));
+            if(accion.esAplicable(estadoActual)){//comprobar si esta explorado ??
+                sc = p.result(estadoActual,accion);
+                System.out.println("-" + (i++) + " - RESULT(" + estadoActual + ","+ accion + ")=" + sc);
+                if(!Explorados.contains(sc)){
+                    System.out.println("-" + (i++) + " - " + sc + " NO explorado");
+                    boolean esta=false;
+                    for (Nodo nf: frontera) {
+                        if(nf.estado == sc){
+                            esta=true;
+                            break;
+                        }
+                    }
+                    if(!esta){
+                        System.out.println("-" + (i++) + " - " + sc + " NO está en la forntera");
+                        frontera.add(new Nodo(sc,padre,accion));
+
+                    }
+                }
+                else{
+                    System.out.println("-" + (i++) + " - " + sc + " ya explorado");
+                }
+
+            }
         }
+        System.out.println("}Frontera expandida");
         return frontera;
-    }*/
+    }
 
     /** Cambiar(copiado de estrategia4), aparece en pag56 teoria */
     @Override
     public Nodo[] soluciona(ProblemaBusqueda p) throws Exception {
 
-        Queue<Estado> frontera = new LinkedList<Estado>();
+        Queue<Nodo> frontera = new LinkedList<>();
         ArrayList<Nodo> listaNodo = new ArrayList<>();
         ArrayList<Estado> explorados = new ArrayList<>();
         Nodo padre = null;
+        Nodo actual ;
         Accion accion;
         Estado estadoActual = p.getEstadoInicial();
-
-        explorados.add(estadoActual);
-        //frontera = sucesores(p, frontera, estadoActual);
+        frontera.add(new Nodo(estadoActual,null,null));
 
         int i = 1;
 
         System.out.println((i++) + " - Empezando búsqueda en " + estadoActual);
-
-        while (!p.esMeta(estadoActual)) {
-            System.out.println((i++) + " - " + estadoActual + " no es meta");
-            Accion[] accionesDisponibles = p.acciones(estadoActual);
-            boolean modificado = false;
-            for (Accion acc : accionesDisponibles) {
-                Estado sc = p.result(estadoActual, acc);
-                System.out.println((i++) + " - RESULT(" + estadoActual + ","+ acc + ")=" + sc);
-                if (!explorados.contains(sc)) {
-                    estadoActual = sc;
-                    System.out.println((i++) + " - " + sc + " NO explorado");
-                    explorados.add(estadoActual);
-                    //frontera = sucesores(p, frontera, estadoActual);
-                    modificado = true;
-                    System.out.println((i++) + " - Estado actual cambiado a " + estadoActual);
-                    accion = acc;
-                    Nodo nodo = new Nodo(estadoActual,padre,accion);
-                    listaNodo.add(nodo);
-                    padre = nodo;
-                    break;
-                }
-                else
-                    System.out.println((i++) + " - " + sc + " ya explorado");
+        while(!p.esMeta(estadoActual)){
+            if(frontera.isEmpty()){
+                throw new Exception("No se ha podido encontrar una solución");
             }
-            if(!modificado) throw new Exception("No se ha podido encontrar una solución");
+            else{
+                actual=frontera.poll();//cojo el nodo padre (primero de la frontera) y lo elimino de la cola
+            }
+            if(p.esMeta(actual.estado)){
+                System.out.println("Es meta"+ estadoActual + actual.estado );
+                listaNodo.add(actual);
+                break;
+            }
+            else{
+                System.out.println((i++) + " - " + estadoActual + " no es meta");
+                explorados.add(estadoActual);
+                frontera=sucesores(p,frontera,estadoActual,actual,explorados);
 
+            }
+            if(frontera.size()>0){
+                estadoActual=frontera.peek().estado;
+                System.out.println((i++) + " - Estado actual cambiado a " + estadoActual);
+            }
+            listaNodo.add(frontera.peek());
         }
+
         System.out.println((i++) + " - FIN - " + estadoActual);
         Nodo[] arrayNodo = new Nodo[listaNodo.size()]; int j=0;
         for (Nodo nodo:listaNodo) {
@@ -72,39 +93,3 @@ public class EstrategiaBusquedaGrafo implements EstrategiaBusqueda {
     }
 
 }
-/*System.out.println((i++) + " - Empezando búsqueda en " + estadoActual);
-        while (!p.esMeta(estadoActual)){
-            if(frontera.isEmpty()){
-                throw new Exception("No se ha podido encontrar una solución");
-            }
-            padre=frontera.get(0);
-            n.add(frontera.get(0));
-            frontera.remove(0);
-            Accion[] accionesDisponibles = p.acciones(estadoActual);
-            for(Accion acc: accionesDisponibles){
-                Estado sc = p.result(estadoActual, acc);
-                System.out.println((i++) + " - RESULT(" + estadoActual + ","+ acc + ")=" + sc);
-                if(p.esMeta(sc)){
-                    n.add(new Nodo(sc,padre,acc));
-                    break;
-                }
-                if(!explorados.contains(estadoActual)){
-                    System.out.println((i++) + " - " + sc + " NO explorado");
-                    explorados.add(estadoActual);}
-                if(!explorados.contains(sc)){
-                    boolean esta=false;
-                    for (Nodo nf: frontera) {
-                        if(nf.estado == sc){
-                            esta=true;
-                            break;
-                        }
-                    }
-                    if(!esta){
-                        frontera.add(new Nodo(sc,padre,acc));
-                    }
-                }
-            }
-            if(frontera.size()>0){
-                estadoActual=frontera.get(0).estado;
-                System.out.println((i++) + " - Estado actual cambiado a " + estadoActual);
-            }*/
