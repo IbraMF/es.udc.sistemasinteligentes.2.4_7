99caf91ca8e5fd4c954b75108d80e9be79900130
Ibrahim Mendez Fernandez <i.mendezf@udc.es>
2022-03-13 16:32:01 +0100

    busqueda grafos: listanodos sin meta repetida + while ahora es loop infinito hasta break;

--- GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/EstrategiaBusquedaGrafo.java
+++ GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/EstrategiaBusquedaGrafo.java
@@ -2,6 +2,7 @@ package es.udc.sistemasinteligentes.ejemplo;
 
 import es.udc.sistemasinteligentes.*;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,9 +17,9 @@ public class EstrategiaBusquedaGrafo implements EstrategiaBusqueda {
         for(Accion accion : accionesDisponibles){
             if(accion.esAplicable(estadoActual)){//comprobar si esta explorado ??
                 sc = p.result(estadoActual,accion);
-                System.out.println("-" + (i++) + " - RESULT(" + estadoActual + ","+ accion + ")=" + sc);
+                System.out.println("\t-" + (i++) + " - RESULT(" + estadoActual + ","+ accion + ")=" + sc);
                 if(!Explorados.contains(sc)){
-                    System.out.println("-" + (i++) + " - " + sc + " NO explorado");
+                    System.out.println("\t-" + (i++) + " - " + sc + " NO explorado");
                     boolean esta=false;
                     for (Nodo nf: frontera) {
                         if(nf.estado == sc){
@@ -27,13 +28,12 @@ public class EstrategiaBusquedaGrafo implements EstrategiaBusqueda {
                         }
                     }
                     if(!esta){
-                        System.out.println("-" + (i++) + " - " + sc + " NO está en la forntera");
+                        System.out.println("\t-" + (i++) + " - " + sc + " NO está en la frontera");
                         frontera.add(new Nodo(sc,padre,accion));
-
                     }
                 }
                 else{
-                    System.out.println("-" + (i++) + " - " + sc + " ya explorado");
+                    System.out.println("\t-" + (i++) + " - " + sc + " ya explorado");
                 }
 
             }
@@ -58,7 +58,7 @@ public class EstrategiaBusquedaGrafo implements EstrategiaBusqueda {
         int i = 1;
 
         System.out.println((i++) + " - Empezando búsqueda en " + estadoActual);
-        while(!p.esMeta(estadoActual)){
+        while(true){
             if(frontera.isEmpty()){
                 throw new Exception("No se ha podido encontrar una solución");
             }
@@ -66,8 +66,7 @@ public class EstrategiaBusquedaGrafo implements EstrategiaBusqueda {
                 actual=frontera.poll();//cojo el nodo padre (primero de la frontera) y lo elimino de la cola
             }
             if(p.esMeta(actual.estado)){
-                System.out.println("Es meta"+ estadoActual + actual.estado );
-                listaNodo.add(actual);
+                System.out.println((i++) + " - " + actual.estado + " es meta" );
                 break;
             }
             else{
@@ -79,8 +78,8 @@ public class EstrategiaBusquedaGrafo implements EstrategiaBusqueda {
             if(frontera.size()>0){
                 estadoActual=frontera.peek().estado;
                 System.out.println((i++) + " - Estado actual cambiado a " + estadoActual);
+                listaNodo.add(frontera.peek());
             }
-            listaNodo.add(frontera.peek());
         }
 
         System.out.println((i++) + " - FIN - " + estadoActual);
